fc77d7b360ad6285e9c473615325d11011c2ef80
Ankit Kumar <ankitbabu647@gmail.com>
2023-03-06 16:36:55 +0530

    Form Application Developed by Ankit today actions

--- app/src/main/java/com/example/formapplication/MainActivity.kt
+++ app/src/main/java/com/example/formapplication/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.formapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.CompoundButton
 import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.formapplication.databinding.ActivityMeterialUiBinding
 
-class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, View.OnClickListener{
+class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, View.OnClickListener,
+    CompoundButton.OnCheckedChangeListener {
     private lateinit var  binding:ActivityMeterialUiBinding
     private lateinit var fname:String
     private lateinit var lname:String
@@ -16,8 +18,9 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, Vi
     private lateinit var altmobileno:String
     private lateinit var email:String
     private lateinit var gender:String
-    lateinit var hobbies:String
-    var arrayList= arrayListOf<String>()
+
+
+    //--------------------------------------------------------------------------------------------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMeterialUiBinding.inflate(layoutInflater)
@@ -25,41 +28,17 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, Vi
         binding.btnsubmit.setOnClickListener(this)
         binding.rbgroup.setOnCheckedChangeListener(this)
 
-    }
-
-    override fun onClick(v: View?) {
-       fname=binding.firstnameinput.editText?.text.toString()
-
-
-        lname=binding.lastnameinput.editText?.text.toString()
-        mobileno=binding.mobilenoinput.editText?.text.toString()
-        if (isPhoneNumberValid(mobileno))
-            Toast.makeText(this," Number is Valid ",Toast.LENGTH_LONG).show()
-        else
-            Toast.makeText(this," Number is InVelid ",Toast.LENGTH_LONG).show()
-        altmobileno=binding.altmobilenoinput.editText?.text.toString()
-        email=binding.emailinput.editText?.text.toString()
-        if(checkphone(mobileno,altmobileno)){
-            " Name :    $fname $lname".also { binding.tvFullname.text = it }
-            " Mobile No :  $mobileno".also { binding.tvMobno.text = it }
-            " Alternative Mobile No : $altmobileno".also { binding.tvAltno.text = it }
-            " E-Mail : $email".also { binding.tvEmail.text = it }
-            " Gender : $gender".also { binding.tvGender.text = it }
-        }
-        else
-        {
-            Toast.makeText(this," Please Enter Alternative Number Different From Main Number  ",Toast.LENGTH_LONG).show()
-            return
-        }
-
-
 
     }
 
+//--------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+    // Here I Use RadioButton Properties
     override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
         when(checkedId){
             R.id.rbbtnmale->{
-               gender=binding.rbbtnmale.text.toString()
+                gender=binding.rbbtnmale.text.toString()
             }
 
             R.id.rbbtnfemale->{
@@ -74,11 +53,47 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, Vi
 
     }
 
+// Here I Use Button Properties
+    //-----------------------------------------------------------------------------------------------
+    @SuppressLint("SuspiciousIndentation")
+    override fun onClick(v: View?) {
+       fname=binding.firstnameinput.editText?.text.toString()
+        lname=binding.lastnameinput.editText?.text.toString()
+        mobileno=binding.mobilenoinput.editText?.text.toString()
+        altmobileno=binding.altmobilenoinput.editText?.text.toString()
+        email=binding.emailinput.editText?.text.toString()
+
+        binding.cb2.setOnCheckedChangeListener(this)
+        if(validationOnNumbers(mobileno,altmobileno)) {
+            " Name :    $fname $lname".also { binding.tvFullname.text = it }
+            " Mobile No :  $mobileno".also { binding.tvMobno.text = it }
+            " Alternative Mobile No : $altmobileno".also { binding.tvAltno.text = it }
+            " E-Mail : $email".also { binding.tvEmail.text = it }
+            " Gender : $gender".also { binding.tvGender.text = it }
+        }
+    else{
+        Toast.makeText(this," Please Enter Alternative Number Different ",Toast.LENGTH_LONG).show()
+            return
+        }
+
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        binding.cb1.isChecked
+    }
+
+    //-----------------------------------------------------------------------------------------------
+
+    //  Here i  Will Use CheckBox Property
+    }
+fun validationOnNumbers(m1:String,m2:String):Boolean{
+    return m1!=m2
 }
-fun checkphone(m1:String,m2:String): Boolean {
-    return m1 != m2
-}
-fun isPhoneNumberValid(phone_number:String):Boolean{
-    val regexPattern="^\\d{10}$"
-    return Regex(regexPattern).matches(phone_number)
-}
+
+
+
+
+
+
+
+
